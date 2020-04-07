d99c9940e990276498b07faa3f42fa61cc33f832
Benjamin Hoppe <hoppeconnect@gmail.com>
2020-04-07 09:25:11 -0400

    added animations and menu

--- App.js
+++ App.js
@@ -6,10 +6,12 @@ import {Ionicons} from "@expo/vector-icons";
 import {NotificationIcon} from "./components/Icons";
 import Logo from "./components/Logo";
 import Course from "./components/Course";
+import Menu from "./components/Menu";
 
 export default function App() {
   return (
     <Container>
+      <Menu />
       <SafeAreaView>
         <ScrollView>
           <TitleBar>

58a0fa87c6f004f4dd61be2c9e2ff1c2c4b5d27c
Benjamin Hoppe <hoppeconnect@gmail.com>
2020-04-10 11:12:11 -0400

    added react-navigation

--- App.js
+++ App.js
@@ -1,32 +1,36 @@
-import React from "react";
-import {createStore} from "redux";
-import {Provider} from "react-redux";
-import HomeScreen from "./screens/HomeScreen";
+import React from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import AppNavigator from './navigator/AppNavigator';
+import {NavigationContainer} from '@react-navigation/native';
 
 const initialState = {
-  action: "",
-  name: "",
+  action: '',
+  name: '',
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "OPEN_MENU":
-      return {action: "openMenu"};
-    case "CLOSE_MENU":
-      return {action: "closeMenu"};
-    case "UPDATE_NAME":
+    case 'OPEN_MENU':
+      return {action: 'openMenu'};
+    case 'CLOSE_MENU':
+      return {action: 'closeMenu'};
+    case 'UPDATE_NAME':
       return {name: action.name};
     default:
       return state;
   }
 };
 
-const store = createStore(reducer);
+const store = createStore (reducer);
 
 const App = () => (
-  <Provider store={store}>
-    <HomeScreen />
-  </Provider>
+  <NavigationContainer>
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  </NavigationContainer>
 );
 
 export default App;
